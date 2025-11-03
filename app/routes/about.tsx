496267b1a830d24162f209560d26e7c80798bf6d
江頭俊亮 <egashirashunsuke@egashirashunsukenoMacBook-Air.local>
2025-11-03 12:10:27 +0900

    add:aboutのページだけ作成

---
+++ app/routes/about.tsx
@@ -0,0 +1,9 @@
+const about = () => {
+  return (
+    <div className="max-w-xl mx-auto p-8">
+      このアプリは、LLMによるUMTP問題学習者に対してのヒントの効果を検証するアプリケーションである。
+    </div>
+  );
+};
+
+export default about;
